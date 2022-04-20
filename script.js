153cb2b972ce58b719f2711cb3b35ea6d9303faf
Luiz Guilherme Bestel <luizgbes@gmail.com>
2022-04-20 16:56:15 -0300

    requisitos 4 e 5

--- script.js
+++ script.js
@@ -22,20 +22,44 @@ function squarePalette () {
     }
 } squarePalette ();
 
-function createPixelBoard (lines) {
+function createPixelBoard (lines, columns) {
 
-    // for(let i = 0; i < 5; i += 1)
-    
-    for (let iline = 0; iline < lines; iline += 1) {
+    for(let icol = 0; icol < columns; icol += 1) {
         let pixelBoard = document.querySelector('#pixel-board');
-        let createPixel = document.createElement('div');
-        createPixel.className = 'pixel';
-        pixelBoard.appendChild(createPixel);
-        createPixel.style.backgroundColor = 'white';
-        createPixel.style.border = '1px solid black';
-        createPixel.style.height = '20px';
-        createPixel.style.width = '20px';
-        createPixel.style.display = 'inline-block'
+        let createPixelLine = document.createElement('div');
+        createPixelLine.className = 'pixel-line';
+        pixelBoard.appendChild(createPixelLine);
+        // createPixelLine.style.backgroundColor = 'purple';
+        // createPixelLine.style.border = '1px solid black';
+        // createPixelLine.style.height = '40px';
+        createPixelLine.style.width = 'auto';
+
+        for (let iline = 0; iline < lines; iline += 1) {
+            // let pixelBoard = document.querySelector('#pixel-board');
+            let pixelBoardLine = document.querySelectorAll('.pixel-line');
+            let createPixel = document.createElement('div');
+            createPixel.className = 'pixel';
+            pixelBoardLine[icol].appendChild(createPixel);
+            createPixel.style.backgroundColor = 'white';
+            createPixel.style.border = '1px solid black';
+            createPixel.style.height = '40px';
+            createPixel.style.width = '40px';
+            createPixel.style.display = 'inline-block'
+        }
     }
 
-} createPixelBoard (5);
+    
+    // for (let iline = 0; iline < lines; iline += 1) {
+    //     // let pixelBoard = document.querySelector('#pixel-board');
+    //     let pixelBoardLine = document.querySelectorAll('.pixel-line');
+    //     let createPixel = document.createElement('div');
+    //     createPixel.className = 'pixel';
+    //     pixelBoardLine[iline].appendChild(createPixel);
+    //     createPixel.style.backgroundColor = 'white';
+    //     createPixel.style.border = '1px solid black';
+    //     createPixel.style.height = '40px';
+    //     createPixel.style.width = '40px';
+    //     createPixel.style.display = 'inline-block'
+    // }
+
+} createPixelBoard (5, 5);
