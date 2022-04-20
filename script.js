da3b3c7111f281d88ff3a7f110b6c3f50250a943
Luiz Guilherme Bestel <luizgbes@gmail.com>
2022-04-20 15:29:12 -0300

    requisito 3

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-let colorPalette = document.getElementById('color-palette');
-let colors = document.querySelectorAll('.color');
-let squareSize = '20px';
+function createBlack () {
 
-for(let i = 0; i < colors.length; i +=1) {
-    colors[i].style.height = squareSize;
-    colors[i].style.width = squareSize;
-    colors[i].style.display = 'inline-block';
-}
+    let colorPalette = document.querySelector('#color-palette');
+    let blackDiv = document.createElement('div');
+    blackDiv.className = 'color';
+    blackDiv.id = 'black';
+    blackDiv.style.backgroundColor = 'black';
+    blackDiv.style.border = '1px solid black';
+    colorPalette.prepend(blackDiv);  
+
+} createBlack ();
+
+function squarePalette () {
+
+    let colors = document.querySelectorAll('.color');
+    let squareSize = '20px';
+
+    for(let i = 0; i < colors.length; i +=1) {
+        colors[i].style.height = squareSize;
+        colors[i].style.width = squareSize;
+        colors[i].style.display = 'inline-block';
+    }
+} squarePalette ();
